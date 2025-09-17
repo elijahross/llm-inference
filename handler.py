ac93977c2282d885a8bee7da523f18dac435e4f0
Elijah Ross <elijah.ross@haseundigel.com>
2025-09-17 23:25:03 +0200

    Update handler.py

--- handler.py
+++ handler.py
@@ -9,10 +9,7 @@ runner = Runner(
         from_uqff=["/runpod-volume/models/llama4-scout-instruct-q4k-0.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-1.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-2.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-3.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-4.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-5.uqff", "/runpod-volume/models/llama4-scout-instruct-q4k-6.uqff"],
         arch=VisionArchitecture.Llama4,
         hf_cache_path="/runpod-volume/hf_cache"
-    ),
-    max_seq_len=12192,
-    max_batch_size=1,
-    max_num_images=4
+    )
 )
 
 def handler(job):
